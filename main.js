dc11576cd989cfdfba3580a161ca72a165b177d4
Yago Frankley <frankleyyago@outlook.com>
2023-03-23 09:06:11 -0300

    Correção tempo mensagem sucesso

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 const formulario = document.getElementById('formulario')
+const campoA = document.getElementById('campoA')
 
 formulario.addEventListener('submit', function(e) {
     e.preventDefault()
 
-    const campoA = document.getElementById('campoA')
     const campoB = document.getElementById('campoB')
     const mensagemSucesso = `Campo A: ${campoA.value} e campo B: ${campoB.value} foram enviados com sucesso`
     const mensagemErro = `O campo B deve ser maior que o campo A`
+
     if (parseInt(campoB.value) > parseInt(campoA.value)) {
-        formulario.submit()
         const containerMensagemSucesso = document.querySelector('.success-message')
         containerMensagemSucesso.innerHTML = mensagemSucesso
         containerMensagemSucesso.style.display = 'block'
+        campoA.value = ''
+        campoB.value = ''
         setTimeout(function() {
             containerMensagemSucesso.style.display = 'none'
-        }, 5000)
+        }, 2500)
     } else {
-        campoA.style.border = '2px solid red'
         document.querySelector('.error-message').style.display = 'block'
         const containerMensagemErro = document.querySelector('.error-message')
         containerMensagemErro.innerHTML = mensagemErro
         containerMensagemErro.style.display = 'block'
+        setTimeout(function() {
+            containerMensagemErro.style.display = 'none'
+        }, 2500)
     }
+
 })
