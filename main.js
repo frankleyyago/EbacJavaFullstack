2bbf4e8892f4af6df3c501d673d8872db6c0fd74
Yago Frankley <frankleyyago@outlook.com>
2023-03-30 18:45:27 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let base = prompt("Digite a base: ");
+
+let altura = prompt("Digite a altura: ");
+
+function area() {
+
+  var area = base * altura;
+
+  return area
+
+}
+
+alert("A área é: " + area())
