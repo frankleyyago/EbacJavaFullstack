f5d3f63501fc1eaa73112a8e2bc750d3edba1d60
Yago Frankley <frankleyyago@outlook.com>
2023-04-22 10:43:30 -0300

    jQuery address mask

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('btn-buscar-cep').addEventListener('click', function() {
-        const xhttp = new XMLHttpRequest()
-        const cep = document.getElementById('cep').value
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`
-
-        xhttp.open('GET', endpoint)
-        xhttp.send()
-    })
-})
-
-// $(document).ready(function() {
-//     $('#btn-search-cep').click(function() {
-//         const cep = $('#cep').val()
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('btn-buscar-cep').addEventListener('click', function() {
+//         const xhttp = new XMLHttpRequest()
+//         const cep = document.getElementById('cep').value
 //         const endpoint = `https://viacep.com.br/ws/${cep}/json`
 
-//         $.ajax(endpoint).done(function(answer) {
-//             const place = answer.place
-//             const neighborhood = answer.neighborhood
-//             const city = answer.city
-//             const state = answer.state
-//             const adress = `${place}, ${neighborhood} - ${city} - ${state}`
-//             $('adress').val(adress)
-//         })
+//         xhttp.open('GET', endpoint)
+//         xhttp.send()
 //     })
 // })
+
+
+$(document).ready(function() {
+    $('#cep').mask('00000-000')
+
+    $('#btn-buscar-cep').click(function() {
+        const cep = $('#cep').val()
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`
+        const botao = $(this)
+        $(botao).find('i').addClass('d-none')
+        $(botao).find('span').removeClass('d-none')
+
+        $.ajax(endpoint).done(function(resposta) {
+            const logradouro = resposta.logradouro
+            const bairro = resposta.bairro
+            const cidade = resposta.localidade
+            const estado = resposta.uf
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`
+            $('endereco').val(endereco)
+
+            setTimeout(function() {
+                $(botao).find('i').removeClass('d-none')
+                $(botao).find('span').addClass('d-none')
+            }, 4000)
+
+        })
+    })
+})
