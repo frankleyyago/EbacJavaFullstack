ac366fdedb3db9aac8048c9bf25ef5a350086d8b
Yago Frankley <frankleyyago@outlook.com>
2023-04-22 12:55:57 -0300

    update html and js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // document.addEventListener('DOMContentLoaded', function() {
-//     document.getElementById('btn-buscar-cep').addEventListener('click', function() {
+//     document.getElementById('btn-seach-cep').addEventListener('click', function() {
 //         const xhttp = new XMLHttpRequest()
 //         const cep = document.getElementById('cep').value
 //         const endpoint = `https://viacep.com.br/ws/${cep}/json`
@@ -9,30 +9,57 @@
 //     })
 // })
 
-
 $(document).ready(function() {
     $('#cep').mask('00000-000')
 
-    $('#btn-buscar-cep').click(function() {
+    $('#btn-search-cep').click(function() {
         const cep = $('#cep').val()
         const endpoint = `https://viacep.com.br/ws/${cep}/json`
-        const botao = $(this)
-        $(botao).find('i').addClass('d-none')
-        $(botao).find('span').removeClass('d-none')
+        const button = $(this)
+        $(button).find('i').addClass('d-none')
+        $(button).find('span').removeClass('d-none')
+        
+        // $.ajax(endpoint).done(function(answer) {
+        //     const place = answer.place
+        //     const neighborhood = answer.neighborhood
+        //     const city = answer.city
+        //     const state = answer.state
+        //     const adress = `${place}, ${neighborhood} - ${city} - ${state}`
+        //     $('adress').val(adress)
+        
+        //     setTimeout(function() {
+        //         $(button).find('i').removeClass('d-none')
+        //         $(button).find('span').addClass('d-none')
+        //     }, 4000)
+        // })
 
-        $.ajax(endpoint).done(function(resposta) {
-            const logradouro = resposta.logradouro
-            const bairro = resposta.bairro
-            const cidade = resposta.localidade
-            const estado = resposta.uf
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`
-            $('endereco').val(endereco)
-
-            setTimeout(function() {
-                $(botao).find('i').removeClass('d-none')
-                $(botao).find('span').addClass('d-none')
-            }, 4000)
-
+        fetch(endpoint)
+        .then(function(answer) {
+            return answer.json()
+        }).then(function(json) {
+            const place = json.place
+            const neighborhood = json.neighborhood
+            const city = json.city
+            const state = json.state
+            const adress = `${place}, ${neighborhood} - ${city} - ${state}`
+            $('adress').val(adress)
         })
+        .catch(function(erro) {
+            alert("Error")
+        })
+        .finally(function() {
+            setTimeout(function() {
+                $(button).find('i').removeClass('d-none')
+                $(button).find('span').addClass('d-none')
+            }, 1000)
+        })
+    })
+
+    $('#request-form').submit(function(e) {
+        e.preventDefault()
+
+        if ($('#nome').val().length == 0) {
+            throw new Error('Digite o nome')
+        }
     })
 })
