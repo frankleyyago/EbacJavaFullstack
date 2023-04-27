e94a889bf1ae1bfdd800eeabc47a4c38f188c855
Yago Frankley <frankleyyago@outlook.com>
2023-04-27 10:22:11 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplication(a, b) {
+    return a * b;
+}
+function sayHello(name) {
+    return `Olá, ${name}`;
+}
+console.log(multiplication(30, 20));
+console.log(sayHello('Yago'));
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplication(a: number, b: number): number {
+    return a * b
+}
+
+function sayHello(name: string): string {
+    return `Olá, ${name}`
+}
+
+console.log(multiplication(30, 20))
+console.log(sayHello('Yago'))
